01d3c3a77c24c789667315aa737495038f7acb89
TVATDCI <tvatdci@gmail.com>
2026-02-17 20:08:27 +0100

    fix: add missing return statement in default case, add SET_LOADING and SET_ERROR actions

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,18 +1,33 @@
 const userReducer = (state, action) => {
-    switch (action.type) {
-      case "SET_USERS":
-        return {
-          ...state,
-          users: action.payload,
-        };
-      case "SET_SELECTED_USER":
-        return {
-          ...state,
-          selectedUser: action.payload,
-        };
-      default:
-        state;
-    }
-  };
-  
-  export default userReducer;
+  switch (action.type) {
+    case "SET_USERS":
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+        error: null,
+      };
+    case "SET_SELECTED_USER":
+      return {
+        ...state,
+        selectedUser: action.payload,
+        loading: false,
+        error: null,
+      };
+    case "SET_LOADING":
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    case "SET_ERROR":
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
